2c6025a4b1b55e137f03125fc556215e596cc02e
Josip K <neksuses@gmail.com>
2020-07-19 22:35:57 +0200

    Preparations for Play Store

--- lib/mainUI.dart
+++ lib/mainUI.dart
@@ -8,28 +8,35 @@ import 'variables.dart';
 class MainUI extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
     return Scaffold(
       backgroundColor: mainColor,
       body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            Row(
+        child: SingleChildScrollView(
+          child: Container(
+            height: size.height - 24.0,
+            child: Column(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: <Widget>[
-                Cat('images/maca1.png'),
-                Cat('images/maca2.png'),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Cat('images/maca1.png'),
+                    Cat('images/maca2.png'),
+                  ],
+                ),
+                FactUI(),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Cat('images/maca3.png'),
+                    Cat('images/maca4.png'),
+                  ],
+                ),
               ],
             ),
-            FactUI(),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Cat('images/maca3.png'),
-                Cat('images/maca4.png'),
-              ],
-            ),
-          ],
+          ),
         ),
       ),
     );
--- lib/variables.dart
+++ lib/variables.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
 const Color mainColor = Color(0xFF27786F);
-const Color darkShadow = Color(0xFF21665e);
-const Color lightShadow = Color(0xFF2d8a80);
+const Color darkShadow = Color(0xFF21665E);
+const Color lightShadow = Color(0xFF2D8A80);
 
 const BoxDecoration neumorphicShadow = BoxDecoration(
   boxShadow: [
