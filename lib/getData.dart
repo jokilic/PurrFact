d04e658f9bfa29150b4b92f1f015951e1d6ac7ed
jokilic <neksuses@gmail.com>
2020-12-05 15:00:55 +0100

    Icon on iOS, Closed #2 & Closed #3

--- lib/getData.dart
+++ lib/getData.dart
@@ -4,7 +4,8 @@ import 'dart:math';
 
 const String url = 'https://cat-fact.herokuapp.com/facts';
 List<String> funFacts = [];
-var data;
+List<String> currentFunFacts;
+List data;
 var response;
 int i;
 int randomNumber;
@@ -22,13 +23,28 @@ class DataGetter {
 
   void parseData(response) {
     data = convert.jsonDecode(response.body)['all'];
-    for (i = 0; i < data.length; i++) {
-      funFacts.add(data[i]['text']);
-    }
+    data.forEach((element) {
+      String currentFact = element['text'];
+
+      // If fact has more than 20 words, store it in the list of facts
+      if (currentFact.length > 20) funFacts.add(currentFact);
+    });
+
+    // Put the facts in the list used for displaying them
+    refillFunFacts();
+
+    // Get a random fact from the list
     randomFunFact();
   }
 
+  List<String> refillFunFacts() => currentFunFacts = [...funFacts];
+
   String randomFunFact() {
+    // Refill the list if there are no more facts
+    if (currentFunFacts.length == 2) refillFunFacts();
+
+    currentFunFacts.remove(fact);
+
     randomNumber = Random().nextInt(funFacts.length);
     fact = funFacts[randomNumber];
     print(fact);
